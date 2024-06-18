f6881f1f879ec726011b516c95c295143270cb43
Jaehyuk Chang <cjaehyuk4zed@gmail.com>
2024-06-18 20:11:50 +0900

    test

--- src/main/java/allofhealth/chat/consumer/WebStreamConsumer.java
+++ src/main/java/allofhealth/chat/consumer/WebStreamConsumer.java
@@ -3,17 +3,19 @@ package allofhealth.chat.consumer;
 import allofhealth.chat.producer.ChatProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-
 public class WebStreamConsumer {
+
     private final WebClient webClient;
     private final ChatProducer producer;
 
+    @Autowired
     public WebStreamConsumer(WebClient.Builder webClientBuilder, ChatProducer producer) {
         this.webClient = webClientBuilder
                 // the baseUrl where the data will be consumed (aka server address).
@@ -26,7 +28,7 @@ public class WebStreamConsumer {
     // Consumes the data coming from the above constructor's baseUrl, and produce it to the Kafka broker
     // The URI should be created by a separate API, with a chatroom number (partition ID)
     //
-    public void consumeAndPublish() {
+    public void consumeStreamAndPublish() {
         webClient.get()
 //        Insert the API endpoint where the sender will send chat messages. This API endpoint is appended to the baseUrl address of the webClient
                 .uri("/stream/recentchange")
--- src/main/java/allofhealth/chat/producer/ChatProducer.java
+++ src/main/java/allofhealth/chat/producer/ChatProducer.java
@@ -1,9 +1,12 @@
 package allofhealth.chat.producer;
 
 import allofhealth.chat.config.Constants;
+import allofhealth.chat.payload.ChatMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,9 +15,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Slf4j
 public class ChatProducer {
     // Key - Value Kafka Template
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, ChatMessage> kafkaTemplate;
 
-    public void sendMessage(String msg){
+    public void sendMessage(ChatMessage msg){
+
+        Message<ChatMessage> message = MessageBuilder
+                .withPayload(msg)
+                .setHeader("partitionKey", msg.getRoomNum())
+                .build();
+
         log.info("Sending message to test-application-topic : {}", msg);
         kafkaTemplate.send(Constants.testTopic, msg);
     }
